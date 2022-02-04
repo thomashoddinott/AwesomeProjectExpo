865ab2f5d9a8c7381a6303b57180746c4c2e09d5
Thomas Hoddinott <thomas@Thomass-MacBook-Air.local>
2022-02-04 17:57:11 +0100

    using flatlist

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -6,9 +6,12 @@ function ColorBox({colorName, hexCode}) {
   const boxColor = {
     backgroundColor: hexCode
   }
+  const textColor = {
+    color: parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1 ? 'black' : 'white'
+  }
   return (
     <View style={[styles.box, boxColor]}>
-      <Text style={styles.boxText}>{colorName}: {hexCode}</Text>
+      <Text style={styles.boxText, textColor}>{colorName}: {hexCode}</Text>
     </View>
   );
 }
