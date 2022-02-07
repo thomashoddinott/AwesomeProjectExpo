1a4eea0760206e3220f5493f640b56c641c11b2b
Thomas Hoddinott <thomas@Thomass-MacBook-Air.local>
2022-02-07 16:34:00 +0100

    navigation

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -1,44 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native';
 
-function ColorBox({colorName, hexCode}) {
-  console.log(colorName, hexCode)
-  const boxColor = {
-    backgroundColor: hexCode
-  }
-  const textColor = {
-    color: parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1 ? 'black' : 'white'
-  }
+const ColorBox = props => {
+  const colorStyle = {
+    backgroundColor: props.hexCode,
+  };
+
+  const textStyle = {
+    color:
+      parseInt(props.hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
+
   return (
-    <View style={[styles.box, boxColor]}>
-      <Text style={styles.boxText, textColor}>{colorName}: {hexCode}</Text>
+    <View style={[styles.box, colorStyle]}>
+      <Text style={[styles.text, textStyle]}>
+        {props.colorName} {props.hexCode}
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   box: {
     padding: 10,
+    borderRadius: 3,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 10
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    elevation: 2,
   },
-  cyan: {
-    backgroundColor: '#2aa198',
-  },
-  blue: {
-    backgroundColor: '#268bd2',
-  },
-  magenta: {
-    backgroundColor: '#d33682'
-  },
-  orange: {
-    backgroundColor: '#cb4b16'
-  },
-  boxText: {
+  text: {
+    fontWeight: 'bold',
     color: 'white',
-    fontWeight: 'bold'
-  }
-})
+  },
+});
 
 export default ColorBox;
